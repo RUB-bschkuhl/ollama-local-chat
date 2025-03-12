0c5f7ea48a285860f5ba640095f33b1e6025dc7c
Bastian Schmidt-Kuhl <bastian.schmidt-kuhl@ruhr-uni-bochum.de>
2025-03-12 18:06:47 +0100

    pdfs

--- Services/Ingestion/DataIngestor.cs
+++ Services/Ingestion/DataIngestor.cs
@@ -49,7 +49,7 @@ public class DataIngestor(
             await foreach (var id in vectorCollection.UpsertBatchAsync(newRecords)) { }
 
             modifiedDoc.Records.Clear();
-            modifiedDoc.Records.AddRange(newRecords.Select(r => new IngestedRecord { Id = r.Key, DocumentId = modifiedDoc.Id }));
+            modifiedDoc.Records.AddRange(newRecords.Select(r => new IngestedRecord { Id = r.Key, DocumentId = modifiedDoc.Id + "-" + DateTime.UtcNow.Ticks }));
 
             if (ingestionCacheDb.Entry(modifiedDoc).State == EntityState.Detached)
             {
